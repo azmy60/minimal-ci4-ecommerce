6ed2a030441d745f6adb39ca57ad53fc11da2a00
Mohammad Azmi <mazmiagus2003@gmail.com>
2021-11-06 18:40:55 +0700

    Initial gulp.js & Tailwind CSS

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const { series, src, dest, watch } = require('gulp')
+const { spawn } = require('child_process')
+const gulpIf = require('gulp-if')
+const postcss = require('gulp-postcss')
+const tailwindcss = require('tailwindcss')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+const browserSync = require('browser-sync')
+const reload = browserSync.reload
+
+// Remove files in public/css
+function clean() {
+  const del = require('del')
+  return del([
+    'public/css/*'
+  ])
+}
+
+// Build css
+function css() {
+  return src('src/css/main.css')
+    .pipe(
+      postcss([
+        tailwindcss(),
+        autoprefixer(),
+        ...process.env.NODE_ENV === 'production' 
+          ? [cssnano()] 
+          : []
+      ])
+    )
+    .pipe(dest('public/css'))
+    .pipe(reload({ stream: true }))
+}
+
+// Run php server + live reload
+function serve() {
+  const phpServer = spawn('php', 'spark serve --port 8000'.split(' '), {stdio: 'inherit'})
+  
+  browserSync({ proxy: '[::1]:8000' })
+
+  // Watch css
+  watch(['src/css/*.css', 'tailwind.config.js'], series(css))
+
+  // Watch views
+  watch('app/Views/**/*.php').on('change', browserSync.reload)
+
+  return phpServer
+}
+
+exports.build = series(clean, css)
+exports.default = series(clean, css, serve)
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // mode: 'jit',
+  purge: [
+    './app/Views/**/*.php',
+    './src/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

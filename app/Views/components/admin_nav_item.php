eb8ce937ebcae6fbb4a77b4d560c607b1201b4b4
Mohammad Azmi <mazmiagus2003@gmail.com>
2021-11-11 05:45:05 +0700

    fix(views): remove deprecated view files

--- app/Views/components/admin_nav_item.php
+++
@@ -1,9 +0,0 @@
-<a <?= $attributes ?? '' ?> x-data="{ myPath: '<?= $path ?>' }" @click="viewPage(myPath)" :class="{ 'text-emerald-400': currentPath === myPath }" href="<?= $path ?>" class="flex items-center gap-4 py-4 pl-4 hover:bg-emerald-900 md:flex-col md:gap-0 md:text-center md:p-0 md:h-20 md:flex md:justify-center lg:flex-row lg:justify-start lg:pl-4 lg:h-auto lg:py-4 lg:gap-4">
-  <svg x-show="currentPath !== myPath" class="w-7 h-7">
-    <use xlink:href="<?= '#ph_' . $icon ?>" />
-  </svg>
-  <svg x-cloak x-show="currentPath === myPath" class="w-7 h-7">
-    <use xlink:href="<?= '#ph_' . $icon . '-bold' ?>" />
-  </svg>
-  <span :class="{ 'font-semibold': currentPath === myPath }" class="text-xl md:text-base lg:text-lg"><?= $name ?></span>
-</a>
--- app/Views/components/loading_spinner.php
+++
@@ -1,1 +0,0 @@
-<div class="w-10 h-10 border-4 border-t-4 rounded-full border-trueGray-200 loader"></div>
